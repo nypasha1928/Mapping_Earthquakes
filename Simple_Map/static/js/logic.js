e459182286a0b2f017a971468c2ea3b1e2ece551
nypasha1928 <71052359+nypasha1928@users.noreply.github.com>
2021-01-20 23:05:08 -0500

    Update logic.js

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -5,11 +5,42 @@ console.log("working");
 let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 
-L.circleMarker([34.0522, -118.2437], {
-  radius: 300,
-  color: "black",
-  fillColor: "#ffffa1"
-}).addTo(map);
+  /// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: "pk.eyJ1IjoibnliYXNoYSIsImEiOiJja2s2MG9qZzAxOW4xMnBudWJudjIwOTlkIn0.tsCsuNDnMOLQhtqgQrtsdw"
+    });
+    
+    // Then we add our 'graymap' tile layer to the map.
+    streets.addTo(map);
+
+    
+// Get data from cities.js
+let cityData = cities;    
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+       radius: city.population/100000,
+       color: "orange",
+       fill: "#ffa500"
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
+  .addTo(map);
+});
+   
+
+
+
+
+
+// L.circleMarker([34.0522, -118.2437], {
+//   radius: 300,
+//   color: "black",
+//   fillColor: "#ffffa1"
+// }).addTo(map);
 
 // Create the map object with a center and zoom level.
 // This method is useful when we need to add multiple tile layers, 
@@ -28,12 +59,3 @@ L.circleMarker([34.0522, -118.2437], {
 //     radius: 300
 //  }).addTo(map);
 
-/// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: "pk.eyJ1IjoibnliYXNoYSIsImEiOiJja2s2MG9qZzAxOW4xMnBudWJudjIwOTlkIn0.tsCsuNDnMOLQhtqgQrtsdw"
-    });
-    
-    // Then we add our 'graymap' tile layer to the map.
-    streets.addTo(map);
