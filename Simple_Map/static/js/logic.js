f163340a160c7162403986580299f9e6978db43c
nypasha1928 <71052359+nypasha1928@users.noreply.github.com>
2021-01-22 17:19:00 -0500

    torontohoods

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -2,7 +2,7 @@
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
-  // accessToken: "pk.eyJ1IjoibnliYXNoYSIsImEiOiJja2s2MG9qZzAxOW4xMnBudWJudjIwOTlkIn0.tsCsuNDnMOLQhtqgQrtsdw"
+  
   accessToken: API_KEY  
 });
 
@@ -10,7 +10,7 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 let satelliteStreets= L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
-  // accessToken: "pk.eyJ1IjoibnliYXNoYSIsImEiOiJja2s2MG9qZzAxOW4xMnBudWJudjIwOTlkIn0.tsCsuNDnMOLQhtqgQrtsdw"
+  
   accessToken: API_KEY
 });    
 
@@ -25,6 +25,8 @@ let baseMaps = {
 let map = L.map('mapid', {
   center: [43.7, -79.3],
   zoom: 11,
+  weight:1,
+  fillColor:"yellow",
   layers: [satelliteStreets]
 })
 
@@ -39,15 +41,20 @@ L.control.layers(baseMaps).addTo(map);
 // Accessing the Toronto neighborhoods GeoJSON URL.
 let torontoHoods = "https://raw.githubusercontent.com/nypasha1928/Mapping_Earthquakes/main/torontoNeighborhoods.json";
 
-// // Create a style for the lines.
-// let myStyle = {
-//   color: "#ffffa1",
-//   weight: 2
-// }
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
 
 // Grabbing our GeoJSON data.
 d3.json(torontoHoods).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJson(data).addTo(map);
+L.geoJson(data , {
+  style: myStyle,
+  onEachFeature: function(feature, layer){
+    layer.bindPopup("<h3>" + "Airline: " +  feature.properties.airline + "</h3> <hr><h3> Destination: " + feature.properties.dst + "</h3>");
+   }
+  }).addTo(map);
 });
