b00b18b477f9289bff7c8fcd7ae992c2ddb9e390
nypasha1928 <71052359+nypasha1928@users.noreply.github.com>
2021-01-22 02:24:30 -0500

    toronto

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,9 +1,9 @@
-  /// We create the tile layer that will be the background of our map.
-  let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: "pk.eyJ1IjoibnliYXNoYSIsImEiOiJja2s2MG9qZzAxOW4xMnBudWJudjIwOTlkIn0.tsCsuNDnMOLQhtqgQrtsdw"
-    });
+/// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  accessToken: "pk.eyJ1IjoibnliYXNoYSIsImEiOiJja2s2MG9qZzAxOW4xMnBudWJudjIwOTlkIn0.tsCsuNDnMOLQhtqgQrtsdw"
+  });
 
 /// We create the tile layer that will be the background of our map.
 let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -21,7 +21,7 @@ let baseMaps = {
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [30, 30],
+  center: [44.0, -80.0],
   zoom: 2,
   layers: [streets]
 })
@@ -34,12 +34,12 @@ L.control.layers(baseMaps).addTo(map);
 // // Then we add our 'graymap' tile layer to the map.
 // streets.addTo(map);
 
-// Accessing the airport GeoJSON URL.
-let airportData ="https://raw.githubusercontent.com/nypasha1928/Mapping_Earthquakes/main/majorAirports.json";
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/nypasha1928/Mapping_Earthquakes/main/torontoRoutes.json";
 
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
 L.geoJson(data).addTo(map);
