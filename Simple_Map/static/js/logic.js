a9acd47d62f82f03327763003a2aaafce0c0ee43
nypasha1928 <71052359+nypasha1928@users.noreply.github.com>
2021-01-22 15:14:49 -0500

    config

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -2,15 +2,17 @@
 let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
-  accessToken: "pk.eyJ1IjoibnliYXNoYSIsImEiOiJja2s2MG9qZzAxOW4xMnBudWJudjIwOTlkIn0.tsCsuNDnMOLQhtqgQrtsdw"
-  });
+  // accessToken: "pk.eyJ1IjoibnliYXNoYSIsImEiOiJja2s2MG9qZzAxOW4xMnBudWJudjIwOTlkIn0.tsCsuNDnMOLQhtqgQrtsdw"
+  accessToken: API_KEY  
+});
 
 /// We create the tile layer that will be the background of our map.
 let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
-  accessToken: "pk.eyJ1IjoibnliYXNoYSIsImEiOiJja2s2MG9qZzAxOW4xMnBudWJudjIwOTlkIn0.tsCsuNDnMOLQhtqgQrtsdw"
-  });    
+  // accessToken: "pk.eyJ1IjoibnliYXNoYSIsImEiOiJja2s2MG9qZzAxOW4xMnBudWJudjIwOTlkIn0.tsCsuNDnMOLQhtqgQrtsdw"
+  accessToken: API_KEY
+});    
 
 
 // Create a base layer that holds both maps.
