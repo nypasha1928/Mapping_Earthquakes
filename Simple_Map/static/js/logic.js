2598bdf805f36093e78502f6c37709ef817f850f
nypasha1928 <71052359+nypasha1928@users.noreply.github.com>
2021-01-22 03:07:05 -0500

    toronto

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,5 +1,5 @@
 /// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
   accessToken: "pk.eyJ1IjoibnliYXNoYSIsImEiOiJja2s2MG9qZzAxOW4xMnBudWJudjIwOTlkIn0.tsCsuNDnMOLQhtqgQrtsdw"
@@ -15,7 +15,7 @@ let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Street: streets,
+  Light: light,
   Dark: dark
 };
 
@@ -23,7 +23,7 @@ let baseMaps = {
 let map = L.map('mapid', {
   center: [44.0, -80.0],
   zoom: 2,
-  layers: [streets]
+  layers: [light]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
