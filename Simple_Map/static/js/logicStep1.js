0637b18e5eec3e2c18302d4a056a36164cd87f7a
nypasha1928 <71052359+nypasha1928@users.noreply.github.com>
2021-01-22 23:48:33 -0500

    Earthquake_past7days

--- Simple_Map/static/js/logicStep1.js
+++ Simple_Map/static/js/logicStep1.js
@@ -18,14 +18,14 @@ let satelliteStreets= L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satel
 // Create a base layer that holds both maps.
 let baseMaps = {
   "Streets": streets,
-  "Satellite streets": satelliteStreets
+  "Satellite": satelliteStreets
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [43.7, -79.3],
-  zoom: 11,
-  layers: [satelliteStreets]
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [streets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -45,16 +45,11 @@ let myStyle = {
   weight: 2
 }
 
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJson(data , {
-  style: myStyle,
-  onEachFeature: function(feature, layer){
-    layer.bindPopup("<h3>" + "Airline: " +  feature.properties.airline + "</h3> <hr><h3> Destination: " + feature.properties.dst + "</h3>");
-   }
-  }).addTo(map);
+L.geoJson(data).addTo(map);
 });
 
 // satellite-streets-v11
